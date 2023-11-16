0ce4c05b0e6d59f01e84917e286aafbf10f2b77c
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2023-11-16 14:27:16 +0500

    added some more functionality

--- package-all-files/packageFiles/__main__.py
+++ package-all-files/packageFiles/__main__.py
@@ -1,7 +1,11 @@
 import sys
 from packageFiles import module
 
-if(len(sys.argv)>1):
-    module.function_for([*sys.argv[1::],'Called from __main__.py with arguments'])
-else:
-    module.function_for(['ABC',123,'9090','Called from __main__.py'])
+def main(): 
+    if(len(sys.argv)>1):
+        module.function_for([*sys.argv[1::],'Called from __main__.py with arguments'])
+    else:
+        module.function_for(['ABC',123,'9090','Called from __main__.py'])
+
+if __name__=='__main__':
+    main()
